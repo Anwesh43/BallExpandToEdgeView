6ee292d5b9b4c4947d61ebf759ca300b518b4320
Anwesh43 <anweshthecool0@gmail.com>
2020-05-02 02:51:40 +0530

    adding constants and libraries

--- ballexpandtoedgeview/src/main/java/com/anwesh/uiprojects/ballexpandtoedgeview/BallExpandToEdgeView.kt
+++ ballexpandtoedgeview/src/main/java/com/anwesh/uiprojects/ballexpandtoedgeview/BallExpandToEdgeView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.ballexpandtoedgeview
 /**
  * Created by anweshmishra on 02/05/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val balls : Int = 2
+val sizeFactor : Float = 2.9f
+val rFactor : Float = 5f
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
